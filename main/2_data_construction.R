2b5c118157682adb7e3dd607086ccca16a572b8e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-15 11:24:48 +1100

    feature engineering

--- main/2_data_construction.R
+++ main/2_data_construction.R
@@ -14,12 +14,12 @@ path <- "data/drivers/"
 speedDist <- function(trip) {
     dist = sqrt(diff(trip[,1])^2 + diff(trip[,2])^2) # distance
     speed <- dist * 3.6
-    quantile(speed, seq(0.05,1, by = 0.05), na.rm=T)
+    #quantile(speed, seq(0.05,1, by = 0.05), na.rm=T)
 }
 
 ### main ###
 date(); d_num <- 0
-main_df <- matrix(0, nrow = length(drivers)*200, ncol = 24, dimnames = list(NULL, NULL))
+main_df <- matrix(0, nrow = length(drivers)*200, ncol = 27, dimnames = list(NULL, NULL))
 for (driver in drivers){
     
     for (trip in trips){
@@ -32,11 +32,39 @@ for (driver in drivers){
         # target
         target <- 0
         # speed
-        feature_speed <- speedDist(trip_data)
+        speed <- speedDist(trip_data)
+        feature_speed <- quantile(speed, seq(0.05,1, by = 0.05), na.rm=T)
+        # sd_speed
+        sd_speed <- sd(speed)
         # avg_speed
-        avg_speed <- mean(feature_speed)
+        avg_speed <- mean(speed)
+        # avg_speed_stop
+        avg_speed <- mean(speed[which(speed >= 1)])
+        # drive_time
+        drive_time <- nrow(trip_data)
+        # standstill_time
+        standstill_time <- sum(which(speed <= 0.1))/drive_time
+        # acceleration/deceleration
+        adceleration <- speed - vector(0,speed[1:(length(speed)-1)])
+        acceleration <- adceleration[which(adceleration>0)]
+        deceleration <- adceleration[which(adceleration<-0)]
+        constant <- adceleration[which(adceleration==0)]
+        ex_acc <- adceleration[which(adceleration>3)]
+        ex_dec <- adceleration[which(adceleration<-3)]
+        # acceleration/deceleration features
+        avg_acc <- mean(acceleration)
+        avg_dec <- mean(deceleration)
+        feature_acc <- quantile(feature_acc, seq(0.05,1, by = 0.05), na.rm=T)
+        feature_dec <- quantile(feature_dec, seq(0.05,1, by = 0.05), na.rm=T)
+        sd_acc <- sd(acceleration)
+        sd_dec <- sd(deceleration)
+        acc_time <- length(acceleration)/length(adceleration)
+        dec_time <- length(deceleration)/length(adceleration)
+        cons_time <- length(constant)/length(adceleration)
+        ex_acc_time <- length(ex_acc)/length(adceleration)
+        ex_dec_time <- length(ex_dec)/length(adceleration)
         # df
-        main_df[d_num,] <- c(driver, trip, t(feature_speed), avg_speed, target)
+        main_df[d_num,] <- c(driver, trip, t(feature_speed), sd_speed, avg_speed, drive_time, standstill_time, target)
         
         if (trip==200) {
             cat(paste0(files, ' | ', date(), ' | ', d_num/(length(drivers)*200))) 
@@ -44,7 +72,7 @@ for (driver in drivers){
     }
 }
 main_df <- data.frame(main_df,stringsAsFactors = F)
-names(main_df) <- c('driver','trip', names(feature_speed), 'avg_speed', 'target')
+names(main_df) <- c('driver','trip', names(feature_speed), 'sd_speed', 'avg_speed', 'drive_time', 'standstill_time', 'target')
 dim(main_df);head(main_df)
 save(main_df, file='data/main_df.RData')
 write.csv(main_df, file = 'data/main_df.csv', quote = F, row.names = F)
