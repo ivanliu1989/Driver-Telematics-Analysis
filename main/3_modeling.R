f515e4de329b103b72155e021257fa9d81708be8
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-16 13:42:13 +1100

    data cleaning

--- main/3_modeling.R
+++ main/3_modeling.R
@@ -9,33 +9,11 @@ head(main_df)
 datadirectory <- 'data/drivers/'
 drivers <- sort(as.numeric(list.files(datadirectory)))
 
-### Near Zero Variance 
-nzv <- nearZeroVar(main_df, saveMetrics= TRUE)
-nzv[nzv$nzv,][1:10,]
-main_df <- main_df[, -77] # nzv: ex_turn
-
-### Identifying Correlated Predictors
-descrCor <- cor(main_df[,-c(1,2,88)])
-summary(descrCor[upper.tri(descrCor)])
-
-highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
-filteredDescr <- filteredDescr[,-highlyCorDescr]
-
-### Linear Dependencies
-comboInfo <- findLinearCombos(main_df)
-comboInfo
-main_df[, -comboInfo$remove]
-
-### Centering and Scaling
-preProcValues <- preProcess(main_df, method = c("center", "scale"))
-main_dfTransformed <- predict(preProcValues, main_df)
-
-### main process
-#Recursive Partitioning and Regression Trees
-set.seed(888)
-nrOfDriversToCompare <- 5
-submission <- data.frame()
-model <- function(driver) {
+##################
+### Classifier ###
+##################
+#Recursive Partitioning and Regression Trees | svmLinear | gbm | rf | glm | nb | nnet | RFlda
+classifier <- function(driver, model=gbm) {
     currentData <- main_df[main_df[,1]==driver,]
     currentData$target <- 1
     
@@ -44,16 +22,26 @@ model <- function(driver) {
     train <- rbind(currentData, refData)
     
     #model
-    g = glm(as.factor(target) ~ ., data=train[,-c(1,2)], family = binomial("logit"))
-    p = predict(g, currentData, type = "response")
+    g <- train(as.factor(target) ~ ., data = train[,-c(1,2)], method = model,trControl = fitControl, 
+               verbose = FALSE, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC")
+    
+    p = predict(g, newdata = currentData[,-c(1,2)], type = "prob")
     
     result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p)
     return(result)
 }
 
-### main loop
+#################
+### Main Loop ###
+#################
+set.seed(888)
+nrOfDriversToCompare <- 5
+fitControl <- trainControl(method = "adaptive_cv",number = 10,repeats = 5,classProbs = TRUE,
+                           summaryFunction = twoClassSummary,adaptive = list(min = 10,alpha = 0.05,method = "BT",complete = TRUE))
+submission <- data.frame()
+
 for (driver in drivers){
-    result <- model(driver)
+    result <- classifier(driver,'gbm')
     print(paste0('driver: ', driver, ' | ' ,date())) 
     
     submission <- rbind(submission, result)
