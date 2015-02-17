a5a056c927b4344c9a9d69c65a8237f70382b6df
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-17 17:12:50 +1100

    retrain model

--- main/3_modeling.R
+++ main/3_modeling.R
@@ -32,6 +32,30 @@ classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
     return(result)
 }
 
+classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
+    currentData <- main_df[main_df[,1]==driver,]
+    currentData$target <- 'Yes'
+    
+    test_num <- sample(drivers[which(drivers!=driver)],300)
+    refData <-  main_df[main_df[,1] %in% test_num,]
+    refData$target <- 'No'
+    train <- rbind(currentData, refData)
+    # 1st
+    g <- train(x = data.matrix(train[,-c(1,2,80)]), y = as.factor(train$target), method = model,trControl = fitControl, 
+               verbose = F, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)
+    
+    p_other <- predict(g, newdata = data.matrix(refData[,-c(1,2,80)]), type = "prob")
+    result_other <- data.frame(refData, prob=p_other$Yes)
+    refData <- head(head(result_other[order(result_other[,'prob'],decreasing = T),],1000)[,-ncol(result_other)])
+    # 2nd
+    colnames(currentData) <- colnames(refData)
+    train <- rbind(currentData, refData)
+    g <- train(x = data.matrix(train[,-c(1,2,80)]), y = as.factor(train$target), method = model,trControl = fitControl, 
+               verbose = F, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)
+    p <- predict(g, newdata = data.matrix(currentData[,-c(1,2,80)]), type = "prob")
+    result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p$Yes)
+}
+
 #################
 ### Main Loop ###
 #################
@@ -42,14 +66,17 @@ set.seed(888)
 fitControl <- trainControl(method = "none",number = 10,repeats = 3,classProbs = TRUE,
                            summaryFunction = twoClassSummary,adaptive = list(min = 4,alpha = 0.05,method = "BT",complete = TRUE))
 gbmGrid <-  expand.grid(mtry = 18)
+gbmGrid <-  expand.grid(interaction.depth = 2,
+                        n.trees = 200,
+                        shrinkage = 0.1)
 submission <- data.frame()
 
 for (driver in drivers){
-    result <- classifier(driver,'rf',5,rfe_var)
+    result <- classifier(driver,'gbm',5,rfe_var)
     print(paste0('driver: ', driver, ' | ' ,date())) 
     
     submission <- rbind(submission, result)
 }
 
-write.csv(submission, file = 'submission_rf.csv', quote = F, row.names = F)
+write.csv(submission, file = 'submission_gbm_retrain.csv', quote = F, row.names = F)
 sum(is.na(submission))
--- main/3_retrain_model.R
+++ main/3_retrain_model.R
@@ -6,7 +6,7 @@ head(main_df)
 datadirectory <- 'data/drivers/'
 drivers <- sort(as.numeric(list.files(datadirectory)))
 driver <- 1
-model <- 'rf'
+model <- 'gbm'
 
 fitControl <- trainControl(method = "none",number = 10,repeats = 3,classProbs = TRUE,
                            summaryFunction = twoClassSummary,adaptive = list(min = 4,alpha = 0.05,method = "BT",complete = TRUE))
@@ -18,16 +18,21 @@ gbmGrid <-  expand.grid(mtry = 18)
 currentData <- main_df[main_df[,1]==driver,]
 currentData$target <- 'Yes'
 
-test_num <- sample(drivers[which(drivers!=driver)],100)
+test_num <- sample(drivers[which(drivers!=driver)],300)
 refData <-  main_df[main_df[,1] %in% test_num,]
 refData$target <- 'No'
 train <- rbind(currentData, refData)
+# 1st
+g <- train(x = data.matrix(train[,-c(1,2,80)]), y = as.factor(train$target), method = model,trControl = fitControl, 
+           verbose = F, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)
 
-system.time(g <- train(as.factor(target) ~ ., data = train[,-c(1,2)], method = model,trControl = fitControl, 
-           verbose = T, preProc = c("center", "scale"),metric = "ROC",tuneGrid = gbmGrid))
-system.time(g <- train(x = data.matrix(train[,-c(1,2,80)]), y = as.factor(train$target), method = model,trControl = fitControl, 
-                       verbose = F, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid))
-
-p_other <- predict(g, newdata = refData[,-c(1,2)], type = "prob")
-result_other <- data.frame(driver_trip=paste0(refData[,1],'_',refData[,2],sep=''), prob=p_other$Yes)
-head(result_other[order(result_other[,2],decreasing = T),],1000)
+p_other <- predict(g, newdata = data.matrix(refData[,-c(1,2,80)]), type = "prob")
+result_other <- data.frame(refData, prob=p_other$Yes)
+refData <- head(head(result_other[order(result_other[,'prob'],decreasing = T),],1000)[,-ncol(result_other)])
+# 2nd
+colnames(currentData) <- colnames(refData)
+train <- rbind(currentData, refData)
+g <- train(x = data.matrix(train[,-c(1,2,80)]), y = as.factor(train$target), method = model,trControl = fitControl, 
+           verbose = F, preProc = c("center", "scale"),tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)
+p <- predict(g, newdata = data.matrix(currentData[,-c(1,2,80)]), type = "prob")
+result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p$Yes)
