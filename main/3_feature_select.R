894350ce4a50fe2e10334655564559de05fb7621
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-17 12:25:25 +1100

    feature selection

--- main/3_feature_select.R
+++ main/3_feature_select.R
@@ -10,7 +10,7 @@ model <- 'gbm'
 
 #model
 fitControl <- trainControl(method = "adaptive_cv",number = 10,repeats = 5,classProbs = TRUE,
-                           summaryFunction = twoClassSummary,adaptive = list(min = 8,alpha = 0.05,method = "BT",complete = TRUE))
+                           summaryFunction = twoClassSummary,adaptive = list(min = 12,alpha = 0.05,method = "BT",complete = TRUE))
 
 currentData <- main_df[main_df[,1]==driver,]
 currentData$target <- 'Yes'
@@ -23,14 +23,19 @@ g <- train(as.factor(target) ~ ., data = train[,-c(1,2)], method = model,trContr
            verbose = T, preProc = c("center", "scale"),metric = "ROC")
 p <- predict(g, newdata = currentData[,-c(1,2)], type = "prob")
 
-# feature selection
-gbmImp <- varImp(g, scale = FALSE)
-gbmImp
-plot(gbmImp, top = 20)
-RocImp <- filterVarImp(x = training[, -ncol(training)], y = training$Class)
-head(RocImp)
+### feature selection
+# var imp
+gbmImp2 <- varImp(g, scale = F)
+gbmImp2
+plot(gbmImp2, top = 100)
 
-set.seed(10)
+a_df <- data.frame(gbmImp2[1])
+nonImpCol <- rownames(a_df)[which(a_df==0)]
+nonImpCol <- gsub("`","",nonImpCol)
+main_df <- main_df[,-which(colnames(main_df) %in% nonImpCol)]
+
+# rfe
+set.seed(888)
 ctrl <- rfeControl(functions = lmFuncs,method = "repeatedcv",
                    repeats = 5,verbose = FALSE)
 lmProfile <- rfe(x, y,sizes = subsets,rfeControl = ctrl)
