143f4c551784efe44ea7d9b76ac98cd740740a46
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-19 09:02:25 +1100

    update

--- main/2_turn_detect.R
+++ main/2_turn_detect.R
@@ -1,5 +1,6 @@
-driver <- 1223
+driver <- 1
 trip <- sample(1:200,9)
+trip <- c(83,102,120,136,148,183,200)
 
 par(mfcol=c(3,3))
 for (i in trip){
@@ -15,30 +16,36 @@ for (i in trip){
     
 }
 
-for (i in trip){
-    files <- paste0(path, driver, '/', i, ".csv")
-    trip_data <- data.matrix(fread(files, header=T, sep="," ,stringsAsFactor=F))
-    trip_data <- Kalman_Filter(trip_data,1,1,10) #Q_metres_per_second = 50*1000/3600
-    
-    cur <- calcCurvature(trip_data,1)
-    speed <- calcSpeed(trip_data)
-    
-    plot(speed)
-    points(speed[which(cur[,3]<=100)],col='red')
-    #lines(a,type = 'o',col='red')    
-    
-}
+# for (i in trip){
+#     files <- paste0(path, driver, '/', i, ".csv")
+#     trip_data <- data.matrix(fread(files, header=T, sep="," ,stringsAsFactor=F))
+#     trip_data <- Kalman_Filter(trip_data,1,1,10) #Q_metres_per_second = 50*1000/3600
+#     
+#     cur <- calcCurvature(trip_data,1)
+#     speed <- calcSpeed(trip_data)
+#     
+#     plot(speed)
+#     points(speed[which(cur[,3]<=100)],col='red')
+#     #lines(a,type = 'o',col='red')    
+#     
+# }
 
+par(mfcol=c(1,1))
+speed_o <- rm.outlier(speed, fill = T, median = T, opposite = FALSE)
+out <- outlier(speed)
+plot(speed)
+lines(speed_o,col='red')
+points(out,col='blue')
 
-driver trip
-83       1   83
-102      1  102
-120      1  120
-136      1  136
-148      1  148
-183      1  183
-200      1  200
-345      2  145
-393      2  193
-551      3  151
-589      3  189
+# driver trip
+# 83       1   83
+# 102      1  102
+# 120      1  120
+# 136      1  136
+# 148      1  148
+# 183      1  183
+# 200      1  200
+# 345      2  145
+# 393      2  193
+# 551      3  151
+# 589      3  189
