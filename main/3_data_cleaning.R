eab2dae5e0c9232a4dcf3cff77de11977c144d7c
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-23 09:18:15 +1100

    remove outlier

--- main/3_data_cleaning.R
+++ main/3_data_cleaning.R
@@ -5,7 +5,7 @@ rm(list=ls());gc()
 require(caret);require(data.table)
 
 # main_df <- data.frame(fread('data/main_df_136features.csv',header = T, stringsAsFactor = F))
-load(file='data/main_df_136features.RData')
+load(file='data/main_df_190features.RData')
 head(main_df)
 
 ##################
@@ -25,10 +25,14 @@ colnames(main_df[,null_col])
 ##############################
 ### Data Range Calibration ###
 ##############################
-
-
-
-
+col_names <- colnames(main_df)
+stat <- c()
+calbri <- sapply(col_names, function(x){
+    rng <- range(main_df[,x],na.rm = T)
+    stat <- c(stat, rng)
+})
+rownames(calbri) <- c('min', 'max')
+calbri
 
 ##########################
 ### Near Zero Variance ###
@@ -60,3 +64,37 @@ main_df[, -comboInfo$remove]
 #############################
 # preProcValues <- preProcess(main_df, method = c("center", "scale"))
 # main_dfTransformed <- predict(preProcValues, main_df)
+
+######################
+### Abnormal Trips ###
+######################
+main_df[which(main_df[,'speed_100_pct']>120),c(1,2)]
+
+speed_90_pct - 2.787522e+02
+speed_95_pct - 3.922603e+02
+speed_100_pct - 587.44258858
+norAcc_100_pct - 53.99385598
+
+# sp outliers > 120 (432)
+driver trip
+113  152 # 2 points
+1080  100 # 2 points
+1634  136 # 2 points
+# 1635   71 large jumps
+1635   88 # 2 points
+# 1635  160 large jumps
+1899  164
+2056   21
+2441   11
+2441   76
+2441  160
+2441  185
+2774   13
+2973  130
+2982   72
+# 3105   15 large jumps
+3562   95
+
+driver <- 1899; trip <- 164; files <- paste0(path, driver, '/', trip, ".csv")
+trip_data <- data.matrix(fread(files, header=T, sep="," ,stringsAsFactor=F))
+plot(trip_data)
