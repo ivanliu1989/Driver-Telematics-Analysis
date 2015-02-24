87e7347e51d6fff8cb0b6460f714b98b81e26b1a
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-25 09:04:55 +1100

    calibration

--- main/4_calibration_results.R
+++ main/4_calibration_results.R
@@ -19,11 +19,14 @@ results <- (fread('submission_rf_0.81715.csv',header = T, stringsAsFactor = F))
 # write.csv(results, file = 'calibra_1.csv', quote = F, row.names = F)
 
 load('Driver-Telematics-Analysis/repeated_trips/repeated_map_thereshold_0.03_driver_1_2173.RData')
+load('Driver-Telematics-Analysis/repeated_trips/repeated_map_thereshold_0.03_driver_2174_3612.RData')
 match_matrix <- match_matrix[which(match_matrix[,1]>0),]
 results[driver_trip %in% match_matrix[,1],prob]
 results[driver_trip %in% match_matrix[,1],prob:=1]
-write.csv(results, file = 'calibra_20250.csv', quote = F, row.names = F)
+write.csv(results, file = 'calibra_all_0.8.csv', quote = F, row.names = F)
 
 ### distance quantile ###
 # qdist <- seq(0.01,1, by = 0.01)
 # quantile(dist, qdist)
+
+results[(driver_trip %in% match_matrix[,1]) & (prob > 0.8),prob:=1]
