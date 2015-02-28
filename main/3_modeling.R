9acb55ec111c10f646e50b01a1bac158ddf9303a
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-28 16:04:36 +1100

    update

--- main/3_modeling.R
+++ main/3_modeling.R
@@ -25,7 +25,7 @@ classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
     
     #model
     g <- train(x = data.matrix(train[,c(features)]), y = as.factor(train$target), method = model,trControl = fitControl, 
-                tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)#,preProc = c("center", "scale"))
+                tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid,preProc = c("center", "scale"))
     p <- predict(g, newdata = data.matrix(currentData[,c(features)]), type = "prob")
     
     result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p$Yes)
@@ -38,19 +38,19 @@ classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
 # library(doMC)
 # registerDoMC(cores = 2)
 load('Driver-Telematics-Analysis/feature_selection/rfe_var_190.RData')
-set.seed(888)
+set.seed(88)
 fitControl <- trainControl(method = "none",number = 10,repeats = 3,classProbs = TRUE,
                            summaryFunction = twoClassSummary,adaptive = list(min = 4,alpha = 0.05,method = "BT",complete = TRUE))
-gbmGrid <-  expand.grid(mtry=17)
+gbmGrid <-  expand.grid(k = 13)
 feature_list <- colnames(main_df[,-c(1,2,172)])
 submission <- data.frame()
 
 for (driver in drivers){
-    result <- classifier(driver,'rf',5,feature_list)
+    result <- classifier(driver,'knn',5,feature_list)
     print(paste0('driver: ', driver, ' | ' ,date())) 
     
     submission <- rbind(submission, result)
 }
 
-write.csv(submission, file = 'submission_rf_172_17.csv', quote = F, row.names = F)
+write.csv(submission, file = 'submission_knn_172_13.csv', quote = F, row.names = F)
 sum(is.na(submission))
