35bd75998fb3a634b66d20edad25818c4c4902cb
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-08 14:31:50 +1100

    update

--- main/3_modeling.R
+++ main/3_modeling.R
@@ -26,7 +26,7 @@ classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
     #model
     
     g <- train(x = data.matrix(train[,c(features)]), y = as.factor(train$target), method = model,trControl = fitControl, 
-               tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid)#, preProc = c("center", "scale", "YeoJohnson"))#expoTrans,repeats = 15, trace = FALSE)
+               tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid, preProc = c("center", "scale", "YeoJohnson"))#expoTrans,repeats = 15, trace = FALSE)
     p <- predict(g, newdata = data.matrix(currentData[,c(features)]), type = "prob")
     
     result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p$Yes)
@@ -42,12 +42,12 @@ registerDoMC(cores = 2)
 set.seed(68)
 fitControl <- trainControl(method = "none",number = 10,repeats = 3,classProbs = TRUE,
                            summaryFunction = twoClassSummary,adaptive = list(min = 4,alpha = 0.05,method = "BT",complete = TRUE))
-gbmGrid <-  expand.grid(mtry=17)
+gbmGrid <-  expand.grid(sigma=0.01546107, C=8)
 feature_list <- colnames(main_df[,-c(1,2,ncol(main_df))])
 submission <- data.frame()
 
 for (driver in drivers){ #avNNet
-    result <- classifier(driver,'rf',5,feature_list)
+    result <- classifier(driver,'svmRadial',5,feature_list)
     print(paste0('driver: ', driver, ' | ' ,date())) 
     
     submission <- rbind(submission, result)
--- main/4_calibration_results.R
+++ main/4_calibration_results.R
@@ -7,7 +7,7 @@ require(data.table)
 # main_df[which(main_df[,'speed_100_pct']>85),c(1,2)]
 # main_df[which(main_df[,'trip_distance']/main_df[,'drive_time']>85),c(1,2)]
 
-results <- (fread('submission_glmnet_172_PCA_semi_0.85030.csv',header = T, stringsAsFactor = F))
+results <- (fread('First_try_52.csv',header = T, stringsAsFactor = F))
 
 # results[which(main_df[,'speed_100_pct']>85),2] <- 0
 # results[which(main_df[,'trip_distance']/main_df[,'drive_time']>85),2] <- 0
@@ -21,10 +21,10 @@ results <- (fread('submission_glmnet_172_PCA_semi_0.85030.csv',header = T, strin
 # load('Driver-Telematics-Analysis/repeated_trips/repeated_map_thereshold_0.03_driver_1_2173.RData')
 # load('Driver-Telematics-Analysis/repeated_trips/repeated_map_thereshold_0.03_driver_2174_3612.RData')
 
-match_matrix <- data.table(match_matrix)
-# match_matrix[,2] <- paste0(strsplit(match_matrix[,1],'_')[[1]][1],'_',match_matrix[,2])
-match_matrix[,2] <- sapply(1:nrow(match_matrix),function(i){
-    paste0(strsplit(match_matrix[i,1],'_')[[1]][1],'_',match_matrix[i,2])})
+# match_matrix <- data.table(match_matrix)
+# # match_matrix[,2] <- paste0(strsplit(match_matrix[,1],'_')[[1]][1],'_',match_matrix[,2])
+# match_matrix[,2] <- sapply(1:nrow(match_matrix),function(i){
+#     paste0(strsplit(match_matrix[i,1],'_')[[1]][1],'_',match_matrix[i,2])})
 # match_matrix[,matched_trip := paste0(strsplit(driver_trip,'_')[[1]][1],'_',matched_trip)]
 # match_matrix_1 <- match_matrix
 # match_matrix_2 <- match_matrix
@@ -35,9 +35,9 @@ load('Driver-Telematics-Analysis/repeated_trips/repeated_map_thereshold_0.03_ALL
 
 # match_matrix <- match_matrix[which(match_matrix[,1]>0),]
 results[driver_trip %in% match_matrix,prob]
-results[,prob:=0]
+# results[,prob:=0]
 results[driver_trip %in% match_matrix,prob:=1]
-write.csv(results, file = 'calibra_trip_matching.csv', quote = F, row.names = F)
+write.csv(results, file = 'First_try_52_trip_match.csv', quote = F, row.names = F)
 
 ### distance quantile ###
 # qdist <- seq(0.01,1, by = 0.01)
