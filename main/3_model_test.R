079fff9cc88a3d5b50a41bdebb93d6358246ded3
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-08 23:33:26 +1100

    update

--- main/3_model_test.R
+++ main/3_model_test.R
@@ -6,19 +6,19 @@ head(main_df)
 datadirectory <- 'data/drivers/'
 drivers <- sort(as.numeric(list.files(datadirectory)))
 driver <- 2048
-model <- 'avNNet'
+model <- 'nnet'
 
 set.seed(888)
 fitControl <- trainControl(method = "adaptive_cv",number = 10,repeats = 5,classProbs = TRUE,
                            summaryFunction = twoClassSummary,adaptive = list(min = 8,alpha = 0.05,method = "BT",complete = TRUE))
 
 currentData <- main_df[main_df[,1]==driver,]
-currentData$target <- 'Yes'
+# currentData$target <- 'Yes'
 test_num <- sample(drivers[which(drivers!=driver)],5)
 refData <-  main_df[main_df[,1] %in% test_num,]
 refData$target <- 'No'
 train <- rbind(currentData, refData)
-feature_list <- colnames(main_df[,-c(1,2,187)])
+feature_list <- colnames(main_df[,-c(1,2,ncol(main_df))])
 
 g <- train(x = data.matrix(train[,c(feature_list)]), y = as.factor(train$target), method = model, trControl = fitControl, 
            metric = "ROC", tuneLength=8, preProc = c("center", "scale", 'Yeo-Johnson'))#, "pca", repeats = 15, trace = FALSE)  #verbose=T,
@@ -26,8 +26,8 @@ g <- train(x = data.matrix(train[,c(feature_list)]), y = as.factor(train$target)
 
 ### Models:
 # 4. knn - k = 13 | (0.8336250) 0.76644 | 0.79242
-# 6. svmLinear | 0.76341 | 0.80957
-# 7. svmRadial - sigma=0.01546107 C=8 | 0.69989
+#* 6. svmLinear | 0.76341 | 0.80957
+#* 7. svmRadial - sigma=0.01546107 C=8 | 0.69989
 # 8. gbm - ntree150,inter4 | n.trees = 450, interaction.depth = 8 and shrinkage = 0.1 
 # | (0.8933900) 0.80510 | retrain: 0.51472 | 0.84145 | 0.83521
 #* 10. rf - mtry17 | 0.81715 / 0.81915 | 0.84880 | 0.84629 /0.84154 | 0.84077 none pre
--- main/3_modeling.R
+++ main/3_modeling.R
@@ -26,7 +26,7 @@ classifier <- function(driver, model='gbm', nrOfDriversToCompare=5, features) {
     #model
     
     g <- train(x = data.matrix(train[,c(features)]), y = as.factor(train$target), method = model,trControl = fitControl, 
-               tuneLength = 6,metric = "ROC", preProc = c("center", "scale", "YeoJohnson"))#,tuneGrid = gbmGrid,expoTrans,repeats = 15, trace = FALSE)
+               tuneLength = 6,metric = "ROC",tuneGrid = gbmGrid, preProc = c("center", "scale","pca"))#,expoTrans,repeats = 15, trace = FALSE)
     p <- predict(g, newdata = data.matrix(currentData[,c(features)]), type = "prob")
     
     result <- data.frame(driver_trip=paste0(currentData[,1],'_',currentData[,2],sep=''), prob=p$Yes)
@@ -47,11 +47,11 @@ feature_list <- colnames(main_df[,-c(1,2,ncol(main_df))])
 submission <- data.frame()
 
 for (driver in drivers){ #avNNet
-    result <- classifier(driver,'svmLinear',5,feature_list)
+    result <- classifier(driver,'svmRadial',5,feature_list)
     print(paste0('driver: ', driver, ' | ' ,date())) 
     
     submission <- rbind(submission, result)
 }
 
-write.csv(submission, file = 'submission_rf_176_semi.csv', quote = F, row.names = F)
+write.csv(submission, file = 'submission_svmRadial_176_semi.csv', quote = F, row.names = F)
 sum(is.na(submission))
